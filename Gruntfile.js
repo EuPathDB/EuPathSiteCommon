23b997996978d3caf9eed89291b66edc85a8c7a8
Dave Falke <dmfalke@gmail.com>
2013-08-27 02:35:14 +0000

    add npm config, grunt config, and build targets
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathSiteCommon/branches/lesscss@57827 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    // Task configuration.
+    less: {
+      options: {
+        paths: ['Site/htdocs/assets/css', '../WDK/View/webapp/wdk/css']
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '../ApiCommonWebsite/Site/htdocs/assets/css',
+          src: ['*.less'],
+          //dest: process.env.HTML + '/assets/css',
+          dest: 'build',
+          ext: '.css'
+        }]
+      }
+    },
+
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '../ApiCommonWebsite/Site/htdocs/assets/css',
+          src: ['*DB.css'],
+          //dest: process.env.HTML + '/assets/css',
+          dest: 'build',
+          ext: '.orig.css'
+        }]
+      }
+    },
+
+    cssmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'build',
+          src: ['*.css'],
+          //dest: process.env.HTML + '/assets/css',
+          dest: 'build',
+          //ext: '.min.css'
+        }]
+      }
+    },
+
+    clean: ['build']
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Default task.
+  grunt.registerTask('default', ['less']);
+
+  grunt.registerTask('validate', ['clean', 'copy', 'less', 'cssmin']);
+
+};
